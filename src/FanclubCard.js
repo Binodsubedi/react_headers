55abce2ef0b0902dd6a56cfd4bc094c298140ce1
Binodsubedi <binodsubedi125@gmail.com>
2023-07-29 11:27:56 +0545

    tag and name added

--- src/FanclubCard.js
+++ src/FanclubCard.js
@@ -7,7 +7,13 @@ const FanclubCard = ({FanclubCard,cName}) => {
     <div className={cName}>
       <div style={{backgroundImage:`url(${FanclubCard.main_image})`}}/>
       <div className='details_container'>
+        <div className='details_container_tag_name'>
+          <p>${FanclubCard.idol_name}</p>
+          <p>${FanclubCard.tag}</p>
+        </div>
+        <div>
         <h4>{FanclubCard.title}</h4>
+        </div>
       </div>
     </div>
   );
